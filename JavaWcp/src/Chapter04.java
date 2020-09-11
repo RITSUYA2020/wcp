87204696f7299c8f5b86c7ff33c65ccbde995c62
RITSUYA2020 <12ritsuya23@gmail.com>
2020-09-11 16:12:00 +0900

    [Finish]基礎編-4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 2;
+
+		String str = "和崎律哉";
+		System.out.println(str);
+
+		str = "福岡ソフトバンクホークス";
+		System.out.println(str);
+
+		System.out.println(LUCKY_NUMBER);
+	}
+}
